4c9d796a547783c8355d1cfa7ae791a0e1424bf7
Pem <114507408+lesspem@users.noreply.github.com>
2026-08-10 15:03:44 +0800

    Add files via upload

--- javdbapp.js
+++ javdbapp.js
@@ -1,7 +1,7 @@
-// NAME: JavDB App 去广告
+// NAME: JavDB App 去广告 & 去公告
 // AUTHOR: lesspem
 // REPO: https://github.com/lesspem/Surge
-// DESC: 清空 JavDB App 的开屏广告、首页/磁链广告位
+// DESC: 清空 JavDB App 的开屏广告、首页/磁链广告位、启动公告弹窗
 
 (() => {
   const url = $request.url;
@@ -15,14 +15,19 @@
   }
 
   if (obj && obj.data) {
-    // 开屏广告 /api/v1/startup
+    // /api/v1/startup: 关闭开屏广告 + 清空公告
     if (url.indexOf('/api/v1/startup') !== -1) {
       if (obj.data.splash_ad) {
         obj.data.splash_ad.enabled = false;
+        obj.data.splash_ad.overtime = 0;
         obj.data.splash_ad.ad = null;
       }
+      if (obj.data.settings) {
+        obj.data.settings.NOTICE = '';
+        obj.data.settings.UPDATE_DESCRIPTION = '';
+      }
     }
-    // 首页顶部 / 磁链页广告 /api/v1/ads
+    // /api/v1/ads: 清空首页顶部 / 磁链页广告
     else if (url.indexOf('/api/v1/ads') !== -1) {
       obj.data.enabled = false;
       obj.data.ads = {};
@@ -31,3 +36,4 @@
 
   $done({ body: JSON.stringify(obj) });
 })();
+
